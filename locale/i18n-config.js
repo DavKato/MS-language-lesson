2ecb8cf5a5e0b3d5e94904fd2c7b71fbaba6a6a8
Davide Kato <davide.0.kato@gmail.com>
2019-12-01 23:39:47 +0800

    Changed default and fallback locale to Japanese

--- locale/i18n-config.js
+++ locale/i18n-config.js
@@ -1,34 +1,34 @@
 export default {
-  baseUrl: 'https://marinasoranzo.com',
+  baseUrl: "https://marinasoranzo.com",
   locales: [
     {
-      code: 'en',
-      iso: 'en-GB',
-      file: 'en-GB.js'
+      code: "en",
+      iso: "en-GB",
+      file: "en-GB.js"
     },
     {
-      code: 'ja',
-      iso: 'ja-JP',
-      file: 'ja-JP.js'
+      code: "ja",
+      iso: "ja-JP",
+      file: "ja-JP.js"
     },
     {
-      code: 'it',
-      iso: 'it-IT',
-      file: 'it-IT.js'
+      code: "it",
+      iso: "it-IT",
+      file: "it-IT.js"
     },
     {
-      code: 'de',
-      iso: 'de-DE',
-      file: 'de-DE.js'
+      code: "de",
+      iso: "de-DE",
+      file: "de-DE.js"
     }
   ],
-  defaultLocale: 'en',
-  strategy: 'prefix_and_default',
+  defaultLocale: "ja",
+  strategy: "prefix_and_default",
   detectBrowserLanguage: {
     alwaysRedirect: true,
-    cookieKey: 'lang',
-    fallbackLocale: 'en'
+    cookieKey: "lang",
+    fallbackLocale: "ja"
   },
   lazy: true,
-  langDir: 'locale/'
+  langDir: "locale/"
 };
